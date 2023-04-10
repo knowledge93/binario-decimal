71950b7a522f3768fb7a3d05dc23ff746b5b72b7
Brayan Trujillo <brayantrujillonieto@gmail.com>
2023-04-10 00:24:05 -0500

    Decimal - Binario / Binario - Decimal  HTML CSS JAVASCRIPT

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
 //OBTENEMOS VARIABLES
-let inputEntrada=document.querySelector("#numEntrada")
-let resultado=document.querySelector("#resultadoFinal")
+let inputEntrada = document.querySelector("#numEntrada");
+let resultado = document.querySelector("#resultadoFinal");
+let opcionConvertir = document.querySelector("#opcionConvertir");
+let title = document.querySelector("#title");
 
-let btnCalcular=document.querySelector("#btn-calcular")
-let btnLimpiar=document.querySelector("#btn-limpiar")
-
+let btnCalcular = document.querySelector("#btn-calcular");
+let btnLimpiar = document.querySelector("#btn-limpiar");
 
 //FUNCIONES
-function limpiarCampos(){
-    inputEntrada.value=""
-    resultado.value=""
-    inputEntrada.focus()
+function limpiarCampos() {
+  inputEntrada.value = "";
+  resultado.value = "";
+  inputEntrada.focus();
+  //opcionConvertir.value="Decimal - Binario"
 }
 
-function convertirBinario(numEntrada){
-    let resultadoCadena=""
-    while(numEntrada!=0){
-        let binario=Math.floor(numEntrada/2)
-        let residuo=numEntrada%2
+function convertirDecimalBinario(numEntrada) {
+  let resultadoCadena = "";
+  while (numEntrada != 0) {
+    let binario = Math.floor(numEntrada / 2);
+    let residuo = numEntrada % 2;
 
-        numEntrada=binario
+    numEntrada = binario;
 
-        resultadoCadena=residuo+resultadoCadena
-    }
-    return resultadoCadena
+    resultadoCadena = residuo + resultadoCadena;
+  }
+  return resultadoCadena;
+}
+
+function convertirBinarioDecinal(numEntrada) {
+  let array = numEntrada.split("");
+  let decimal = 0;
+
+  for (let i = 0; i <= array.length - 1; i++) {
+    decimal += array[i] * Math.pow(2, array.length - i - 1);
+  }
+
+  return decimal;
+}
+
+function updateVal(e) {
+  title.textContent = e.target.value;
+  limpiarCampos();
 }
 
 //EVENTOS
-btnCalcular.addEventListener("click",()=>{
-    resultado.value=convertirBinario(inputEntrada.value)
-})
+opcionConvertir.addEventListener("change", updateVal);
 
-btnLimpiar.addEventListener("click",()=>{
-    limpiarCampos()
-})
+btnLimpiar.addEventListener("click", () => {
+  limpiarCampos();
+});
 
-
+btnCalcular.addEventListener("click", () => {
+  if (inputEntrada.value == "") {
+    alert("Colocar un número ya sea en Decimal o Binario");
+  } else {
+    switch (opcionConvertir.value) {
+      case "Decimal - Binario":
+        resultado.value = convertirDecimalBinario(inputEntrada.value);
+        break;
+      case "Binario - Decimal":
+        resultado.value = convertirBinarioDecinal(inputEntrada.value);
+        break;
+      default:
+        alert("No existe esta opcion");
+    }
+  }
+});
